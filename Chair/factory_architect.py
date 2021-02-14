5069cf93fceb8743af9ce9fce56af41222b40ae8
Haakon Bakke <haakonharaldson@hotmail.com>
2021-02-14 21:30:38 +0100

    functioning factory overview UI

--- Chair/factory_architect.py
+++ Chair/factory_architect.py
@@ -26,23 +26,23 @@ class FactoryHandler(BaseHTTPRequestHandler):
         s.end_headers()
        
 
-        jsonData = getOrder()
+        jsonData = getChairs()
         chair_list = parseJson(jsonData)
+        json_order_data = getOrder()
+        order_list = parseJsonOrder(json_order_data)
         #makeDFA(chair_list)
-        html_chair_data = OrderOverView(chair_list)
+        html_chair_data = OrderOverView(chair_list, order_list)
         html_code = getHTMLstring("factory_overview.html")
         html_code = html_code.replace("<p> No orders yet </p>", html_chair_data)
         s.wfile.write(bytes(html_code, "utf-8"))
 
         str_path = s.path
 
-def getOrder():
+def getChairs():
     chair_params = { 'name':0, 's_width': 0, 's_depth': 0, 'a_th': 0, 'with_arm': 0, 'with_back': 0,
              'back_height': 0, 'with_top': 0, 'top_th': 0, 'with_mid': 0, 'mid_th': 0,
              'with_bot': 0, 'bot_th': 0 , 'leg_height': 0, 'leg_th': 0, 'with_taper': 0,
-             'spindles': 0  }
-    
-    
+             'spindles': 0  }    
     where_str = '''?a_chair a kbe:chair. \n ''' 
     select_str =""
     for key in chair_params:
@@ -65,6 +65,7 @@ def getOrder():
     #print("JSON", json_data)
     return json_data
 
+
 def parseJson(json_data): #returns an array with parameters
     chair_parms = { 'name':0, 's_width': 0, 's_depth': 0, 'a_th': 0, 'with_arm': 0, 'with_back': 0,
              'back_height': 0, 'with_top': 0, 'top_th': 0, 'with_mid': 0, 'mid_th': 0,
@@ -79,10 +80,50 @@ def parseJson(json_data): #returns an array with parameters
             chair_parms[key] = json_data['results']['bindings'][x][key]['value']
         dic_copy = chair_parms.copy()
         chair_list.append(dic_copy)
-    print("Chair list",chair_list)    
+    #print("Chair list",chair_list)    
     return chair_list
 
-def OrderOverView(chair_list):
+def getOrder():
+    order_params = { 'name': 0, 'quantity': 0, 
+                     'email': 0, 'status': 0  }
+    
+    where_str = '''?a_order a kbe:order. \n ''' 
+    select_str =""
+    for key in order_params:
+        select_str += ' ?'+key
+        where_str += ' ?a_order kbe:'+key+'' ' ?'+key+ '. \n'  
+
+    URL = "http://127.0.0.1:3030/kbe/query"
+    QUERY = '''
+            PREFIX kbe: <http://www.kbe.com/chairs.owl#>
+            SELECT '''+select_str+ '''
+            WHERE {
+               '''+where_str+'''
+            }
+            '''
+   # print("QUERY::", QUERY)
+    PARAMS = {'query':QUERY}
+    response = requests.post(URL,data=PARAMS)
+    #print("Result of query:", response.text)
+    json_order_data = response.json()
+    #print("JSON", json_data)
+    return json_order_data
+def parseJsonOrder(json_order_data): #returns an array with parameters
+    order_params = { 'name': 0, 'quantity': 0, 
+                     'email': 0, 'status': 0  }
+    order_list = []
+    #get sizes
+    num_of_chairs = len(json_order_data['results']['bindings'])
+
+    for x in range(num_of_chairs):
+        for key in order_params:
+            order_params[key] = json_order_data['results']['bindings'][x][key]['value']
+        dic_copy = order_params.copy()
+        order_list.append(dic_copy)
+    print("order list",order_list)    
+    return order_list
+
+def OrderOverView(chair_list, order_list):
     #create html string to update factory overview table
     chair_parms = { 'name':0, 's_width': 0, 's_depth': 0, 'a_th': 0, 'with_arm': 0, 'with_back': 0,
              'back_height': 0, 'with_top': 0, 'top_th': 0, 'with_mid': 0, 'mid_th': 0,
@@ -94,8 +135,8 @@ def OrderOverView(chair_list):
     Msg = ''
     for x in range(len(chair_list)):
         Msg +='<tr>'
-        for key in chair_list[x]:
-            Msg += '<td>'+chair_list[x][key]+'</td>'
+       # for key in chair_list[x]:
+        Msg += '<td>'+chair_list[x]['name']+'</td>''<td>'+order_list[x]['name']+'</td>''<td>'+order_list[x]['email']+'</td>''<td>'+order_list[x]['quantity']+'</td>''<td>'+order_list[x]['status']+'</td>'
         Msg += '</tr>'
     
     return Msg
