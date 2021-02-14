798930c1bdf4af49648b710f2c2e9bdc38e32818
lerausenco <usencolera@gmail.com>
2021-02-14 16:00:15 +0100

    Chair list added

--- Chair/factory_architect.py
+++ Chair/factory_architect.py
@@ -27,8 +27,8 @@ class FactoryHandler(BaseHTTPRequestHandler):
        
 
         jsonData = getOrder()
-        chair_data = parseJson(jsonData)
-        #makeDFA(table_data)
+        chair_list = parseJson(jsonData)
+        makeDFA(chair_list)
         #html_chair_data = OrderOverView(chair_data)
         html_code = getHTMLstring("factory_overview.html")
         #html_code = html_code.replace("<p> No orders yet </p>", html_chair_data)
@@ -41,26 +41,23 @@ def getOrder():
              'back_height': 0, 'with_top': 0, 'top_th': 0, 'with_mid': 0, 'mid_th': 0,
              'with_bot': 0, 'bot_th': 0 , 'leg_height': 0, 'leg_th': 0, 'with_taper': 0,
              'spindles': 0 }
-    chair_param = ['name', 's_width', 's_depth', 'a_th', 'with_arm', 'with_back' \
-                'back_height', 'with_top', 'top_th', 'with_mid', 'mid_th'\
-                'with_bot', 'bot_th', 'leg_height', 'leg_th', 'with_taper'\
-                'spindles']
     
-    #chair_name = str(chair_param[0]) # + "x" +str(chair_param[1])
+    
     where_str = '''?a_chair a kbe:chair. \n ''' 
-
-    for key in chair_params:    
+    select_str =""
+    for key in chair_params:
+        select_str += ' ?'+key
         where_str += ' ?a_chair kbe:'+key+'' ' ?'+key+ '. \n'  
 
     URL = "http://127.0.0.1:3030/kbe/query"
     QUERY = '''
             PREFIX kbe: <http://www.kbe.com/chairs.owl#>
-            SELECT ?name ?s_width ?s_depth ?a_th ?with_arm ?with_back ?back_height ?with_top ?top_th ?with_mid ?mid_th ?with_bot ?bot_th ?leg_height ?leg_th ?with_taper ?spindles
+            SELECT '''+select_str+ '''
             WHERE {
                '''+where_str+'''
             }
             '''
-    #print("QUERY::", QUERY)
+    print("QUERY::", QUERY)
     PARAMS = {'query':QUERY}
     response = requests.post(URL,data=PARAMS)
     #print("Result of query:", response.text)
@@ -73,19 +70,18 @@ def parseJson(json_data): #returns an array with parameters
              'back_height': 0, 'with_top': 0, 'top_th': 0, 'with_mid': 0, 'mid_th': 0,
              'with_bot': 0, 'bot_th': 0 , 'leg_height': 0, 'leg_th': 0, 'with_taper': 0,
              'spindles': 0 }
+    chair_list = []
     #get sizes
-    num_of_params = len(chair_parms)
     num_of_chairs = len(json_data['results']['bindings'])
-    
-    #print('printout from json: ', json_data['results']['bindings'][1]['name']['value'])
+
     for x in range(num_of_chairs):
         for key in chair_parms:
             chair_parms[key] = json_data['results']['bindings'][x][key]['value']
-    print('dictionary with parms', chair_parms)
-    chair_data = chair_parms
-    return chair_data
+        chair_list.append(chair_parms)
+    print("Chair list",chair_list)    
+    return chair_list
 
-def OrderOverView(chair_data):
+def OrderOverView(chair_list):
     #create html string to update factory overview table
     Msg = ''
     for i in range(len(table_data)):
@@ -96,23 +92,27 @@ def OrderOverView(chair_data):
     
     return Msg
 
-def makeDFA(table_data):
-    PARAM_LIST = ['<TABLE_NAME>','<TL_HEIGHT>','<TL_THICKNESS>','<TL_WIDTH>', '<TT_DEPTH>','<TT_THICKNESS>','<TT_WIDTH>']
-
-    template_path = "Templates\\Table.dfa"
+def makeDFA(chair_list):
+    
+    template_path = "Templates\\Chair_base.dfa"
     f = open(template_path, "r")
     dfa_txt = f.read()
 
-    for i in range(len(table_data)): 
-        if (not os.path.isfile("Orders\\"+table_data[i][0]+".dfa")): #check if file already exists
-            #create new dfa file with table name
-            order_data = table_data[i]
-            order_dfa = open("Orders\\"+order_data[0]+".dfa", "w")
-            #write parameters into dfa file
-            for i in range(len(PARAM_LIST)):
-                dfa_txt = dfa_txt.replace(PARAM_LIST[i], order_data[i])
+    for i in range(len(chair_data)):
 
+        if (not os.path.isfile("Orders\\"+chair_data[i]['name']+".dfa")): #check if file already exists
+            
+            #create new dfa file with chair name
+            order_dfa = open("Orders\\"+chair_data[i]['name']+".dfa", "w")
+
+
+            #write parameters into dfa file
+
+            for key in chair_data[i]:
+                dfa_txt = dfa_txt.replace("<"+key+">", chair_data[i][key])    
             order_dfa.write(dfa_txt)
+
+
             order_dfa.close()
 
     return
