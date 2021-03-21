67433a10078ea2400021c1c89de987986ac5ae25
lerausenco <usencolera@gmail.com>
2021-03-21 22:31:25 +0100

    working feedback image

--- Welding/space_for_welding_gun.py
+++ Welding/space_for_welding_gun.py
@@ -1,36 +1,72 @@
 from utils.image_process import open_image, preprocess
+import cv2
 import numpy as np
 import copy
 
-def find_space_for_welder(array,length,width): #,diameter
-    row = 0
-  #  print("First row", array[row,:])
-  #  print("First coloumn", array[:,0])
-  #  print("number of couloumns ", len(array[row,:]))
-  #  print("number of rows  ", len(array[:,0]))
-  #  print(array.shape[1])
+def find_space_for_welder(img_array,model_height,model_length, wall_height ,gun_diam, gun_length):
 
-    a = diameter
+  """
+      Checks if maze is weldable.
+      args:
+          img_array [numpy array] - image containing geometry in img_array form
+          model__height [int] - height of model as shown on UI in m
+          model_length [int] - length of model as shown on UI in m
+          wall_height [int] - height of walls to weld in m
+          gun_diam: [int] - diameter of welding gun in mm
+          gun_length: [int] - length of welding gun in mm 
+      returns:
+          feedback_array [numpy array]
+  """
+  #mm and m conversion
+  if wall_height > gun_length:
+      return None
 
+  coords = copy.deepcopy(img_array)
+   
 
-    coords = copy.deepcopy(array)
-    x = np.linspace(0, width, num=coords.shape[0])
-    y = np.linspace(0,length, num=coords.shape[1])
+    #convert welding gun size to pixel size
+  d_pixels_x = int((gun_diam/1000)/model_length * img_array.shape[0])
+  d_pixels_y = int((gun_diam/1000)/model_height * img_array.shape[1])
+  print("gun size", d_pixels_x, " ", d_pixels_y)
+  #d_pixels_x = 5
+  #d_pixels_y = 5
+  #d_pixels = 3
+  if d_pixels_x == 0:
+    d_pixels_x = 1
+  if d_pixels_y == 0:
+    d_pixels_y = 1
+    #make check array
+  gun_array = np.zeros((d_pixels_y,d_pixels_x))
     
-    
-    print("x: ", x)
-    print("y: ", y)
-    #temp_coord = np.array( [0, 0] )
-    #coords.tolist()
-    print(coords)
-    
-    #sec[2,2] = [ x[2], y[2] ]
-    #print(coords[2,2])
-    for row in range(coords.shape[0]):
-        for col in  range(coords.shape[1]):
-            sec = coords.astype(int)[row:row+2,col:col+2]
-            sec[row,col] = [ x[col], y[row] ]
-            #coords
-    print(coords)
-    return coords
+  feedback_array = np.zeros(img_array.shape)
+  
+  for i in range(d_pixels_x):
+      #start scanning from 0th...d_pixeles'th index in array
+    print("I", i)  
+    for row in range(i, coords.shape[0]-d_pixels_y, d_pixels_y):
+        for col in  range(coords.shape[1]-d_pixels_x):
+            sec = coords.astype(int)[row:row+d_pixels_y,col:col+d_pixels_x]
 
+            if np.array_equal(sec,gun_array):
+              #mark section as ok
+              feedback_array[row:row+d_pixels_y, col:col+d_pixels_x] = np.ones((d_pixels_y, d_pixels_x)) 
+              
+
+  return feedback_array
+    
+  
+"""
+  for i in range(d_pixels_y):
+    for col in range(i, coords.shape[1]-d_pixels_x, d_pixels_x):
+      for row in range(coords.shape[0]-d_pixels_y):
+      
+        sec = coords.astype(int)[row:row+d_pixels_y,col:col+d_pixels_x]
+        
+        if np.array_equal(sec,gun_array):
+          #mark section as ok
+          feedback_array[row:row+d_pixels_y, col:col+d_pixels_x] = np.ones((d_pixels_y, d_pixels_x))   
+
+  
+ 
+  return feedback_array
+          """
