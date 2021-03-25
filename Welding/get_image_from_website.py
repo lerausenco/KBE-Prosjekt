8566bcaf2c5df935efc6b19cdb1bdc104bf45f66
Haakon Bakke <haakonharaldson@hotmail.com>
2021-03-25 12:06:06 +0100

    Download images from website

---
+++ Welding/get_image_from_website.py
@@ -0,0 +1,69 @@
+#Code from: https://www.thepythoncode.com/article/download-web-page-images-python
+
+import requests
+import os
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin, urlparse
+
+def is_valid(url):
+    """
+    Checks whether `url` is a valid URL.
+    """
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+    
+
+def get_all_images(url):
+    """
+    Returns all image URLs on a single `url`
+    """
+    soup = bs(requests.get(url).content, "html.parser")
+    urls = []
+    for img in tqdm(soup.find_all("img"), "Extracting images"):
+        img_url = img.attrs.get("src")
+        if not img_url:
+            # if img does not contain src attribute, just skip
+            continue
+    # make the URL absolute by joining domain with the URL that is just extracted
+        img_url = urljoin(url, img_url)
+        try:
+            pos = img_url.index("?")
+            img_url = img_url[:pos]
+        except ValueError:
+            pass
+
+    # finally, if the url is valid
+        if is_valid(img_url):
+            urls.append(img_url)
+    return urls
+
+def download(url, pathname):
+    """
+    Downloads a file given an URL and puts it in the folder `pathname`
+    """
+    # if path doesn't exist, make that path dir
+    if not os.path.isdir(pathname):
+        os.makedirs(pathname)
+    # download the body of response by chunk, not immediately
+    response = requests.get(url, stream=True)
+    # get the total file size
+    file_size = int(response.headers.get("Content-Length", 0))
+    # get the file name
+    filename = os.path.join(pathname, url.split("/")[-1])
+    # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
+    progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        for data in progress:
+            # write data read to the file
+            f.write(data)
+            # update the progress bar manually
+            progress.update(len(data))
+
+def mainfunc(url, path):
+    # get all images
+    imgs = get_all_images(url)
+    for img in imgs:
+        # for each image, download it
+        download(img, path)
+#mainfunc("https://yandex.com/images/", "yandex-images")
