3c41a0d43ee86382d9344cd9cfe98ea282d10faa
lerausenco <usencolera@gmail.com>
2021-03-25 16:33:58 +0100

    added img arg, not used yet

--- Welding/run.py
+++ Welding/run.py
@@ -3,12 +3,14 @@ from utils.my_handler import myHandler, ThreadedHTTPServer
 import time
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="localhost", help="Provide host name")
     parser.add_argument("--port", type=int, default=1024, help="Provide port number")
-
+    parser.add_argument("--img", type=str, default="img/maze.png", help="Provide file path")
     args = parser.parse_args()
+
 
     server = ThreadedHTTPServer((args.host, args.port), myHandler)
     print(time.asctime(), "Server running at %s:%s" % (args.host, args.port))
