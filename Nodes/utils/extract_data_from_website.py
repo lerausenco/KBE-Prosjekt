8e5e8cea415e91473837ca886519d53d8c6cc17e
Haakon Bakke <haakonharaldson@hotmail.com>
2021-04-12 18:53:42 +0200

    added doc-string to functions and a few comments

--- Nodes/utils/extract_data_from_website.py
+++ Nodes/utils/extract_data_from_website.py
@@ -6,14 +6,20 @@ import numpy as np
 
 
 def web_table_to_dictionary():
+    """
+           Reads website and find the relevant tables. 
+           Extract the values and names and stores them in a dictionary named "data" and returns the data
+
+        """
+
     url = "https://tradingeconomics.com/commodity/steel"
     url_currency = "https://tradingeconomics.com/currencies"
     metal_table = pd.read_html(url)[4]
     currency_table = pd.read_html(url_currency)[0]
     data = {}
-    data[currency_table['Major'][5]] = currency_table['Price'][5]
+    data[currency_table['Major'][5]] = currency_table['Price'][5] #extracts the exchange value for USDCNY
     for i in range(len(metal_table)):
-        data[metal_table['Industrial'][i]] = metal_table['Price'][i]
+        data[metal_table['Industrial'][i]] = metal_table['Price'][i] # Extracts all the metal names and corresponding price
     return data
 #print(metal_table['Industrial'][3], metal_table['Price'][3])
 #print(currency_table)
@@ -24,17 +30,28 @@ data = web_table_to_dictionary()
 #print(web_table_to_dictionary())
 
 def find_dollar_cost_per_kg(data, metal):
+    """
+           Returns the live cost of a material in USD/kg
+
+           args:
+                data: dictionary with data crated from web_table_to_dictionary()
+                metal: name of the material you want to know the cost price of
+                Implemented: Aluminium, Steel, Copper
+
+        """
     if metal == "Aluminium":
-        key = "Aluminum USD/T"
-        return data[key]/1000
+        key = "Aluminum USD/T" 
+        return data[key]/1000  #Aluminium is stored in USD/ton so dividing by 1000 to get USD/kg
     elif metal == "Steel":
-        key = "Steel CNY/T"
-        return data[key]/(data['USDCNY']*1000)
+        key = "Steel CNY/T" #S is stored in CNY/ton so need to divide by 1000 and the exchange rate for USDCNY to get USD/kg
+        return data[key]/(data['USDCNY']*1000) 
     elif metal == "Copper":
-        key = "Copper USD/Lbs"
-        return data[key]/0.45
+        key = "Copper USD/Lbs" #Copper is stored in USD/pound so dividing by 0.45  to get USD/kg
+        return data[key]/0.45 
     else:
+        print("Material not implemented")
         return False
+
 lst_metals = ["Aluminium","Steel","Copper"]
 for key in lst_metals:
     print("Price for: ", key, np.round(find_dollar_cost_per_kg(data,key),2), "USD/kg")
--- Nodes/utils/find_correct_material.py
+++ Nodes/utils/find_correct_material.py
@@ -1,11 +1,6 @@
 from extract_data_from_website import find_dollar_cost_per_kg, web_table_to_dictionary
 data = web_table_to_dictionary()
 materials=[]
-#materials.append( {'name':'E-glass/Epoxy'         , 'rho':2.0, 'E1':  40, 'E2': 10,'XT':1000, 'priceUSD_per_kg':1000} )
-#materials.append( {'name':'S-glass/Epoxy'         , 'rho':2.0, 'E1':  48, 'E2': 11,'XT':1300, 'priceUSD_per_kg':1000} )
-#materials.append( {'name':'Kevlar-49/Epoxy'       , 'rho':1.4, 'E1':  73, 'E2':  5,'XT':1400, 'priceUSD_per_kg':1000} )
-#materials.append( {'name':'Carbon/Epoxy(a)'       , 'rho':1.6, 'E1': 130, 'E2': 10,'XT':1800, 'priceUSD_per_kg':1000} )
-#materials.append( {'name':'Carbon/Epoxy(b)'       , 'rho':1.8, 'E1': 330, 'E2':  8,'XT':1000, 'priceUSD_per_kg':1000} )
 materials.append( {'name':'Steel'                 , 'rho':7.8, 'E1': 210, 'E2':220,'XT':600, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Steel"), 'fracture_thoughness':55, 'yield_strength':300} )
 materials.append( {'name':'Aluminium'             , 'rho':2.7, 'E1':  70, 'E2': 70,'XT':310, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Aluminium"), 'fracture_thoughness':33, 'yield_strength':270} )
 materials.append( {'name':'Copper alloy'          , 'rho':8.3, 'E1':  120, 'E2': 120,'XT':345, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Copper"), 'fracture_thoughness':1, 'yield_strength':310} )
@@ -16,6 +11,15 @@ materials.append( {'name':'Bamboo '               , 'rho':1.16, 'E1':  25, 'E2':
 
 
 def find_stiffest_and_lightest(mat1,mat2):
+    """
+           Finds the stiffest and ligtest material
+
+           args: 
+                two materials as dictionary - mat1, mat2
+
+           returns:
+                Print message and returns the best fit material
+        """
     if  (mat1['E1']/mat1['rho']) > (mat2['E1']/mat2['rho']):
         print(mat1['name'], "Is the stiffest and lightest")
         return mat1
@@ -31,6 +35,15 @@ print(find_stiffest_and_lightest(materials[0],materials[1]))
 
 
 def find_strongest_and_lightest(mat1,mat2):
+    """
+           Finds the strongest and ligtest material
+
+           args: 
+                two materials as dictionary - mat1, mat2
+
+           returns:
+                Print message and returns the best fit material
+        """
     if  (mat1['XT']/mat1['rho']) > (mat2['XT']/mat2['rho']):
         print(mat1['name'], "Is the strongest and lightest")
         return mat1
@@ -44,6 +57,16 @@ def find_strongest_and_lightest(mat1,mat2):
 print(find_strongest_and_lightest(materials[0],materials[1]))
 
 def find_stiff_and_cheap(mat1,mat2):
+    """
+           Finds the stiffest and cheapest material
+
+           args: 
+                two materials as dictionary - mat1, mat2
+
+           returns:
+                Print message and returns the best fit material
+        """
+
     if  (mat1['E1']/(mat1['rho'] * mat1['priceUSD_per_kg'])) > (mat2['E1']/(mat2['rho'] * mat2['priceUSD_per_kg'])):
         print(mat1['name'], "Is the stiffest and cheapest per mass")
         return mat1
