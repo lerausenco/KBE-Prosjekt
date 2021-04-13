3f3668a8480ff37fe1ddfc5d1e07463b0badb71c
lerausenco <usencolera@gmail.com>
2021-04-13 21:15:54 +0200

    moved imports

--- Nodes/utils/find_correct_material.py
+++ Nodes/utils/find_correct_material.py
@@ -1,13 +1,5 @@
-from extract_data_from_website import find_dollar_cost_per_kg, web_table_to_dictionary
+#from extract_data_from_website import find_dollar_cost_per_kg, web_table_to_dictionary
 
-data = web_table_to_dictionary()
-
-materials=[]
-materials.append( {'name':'Steel'                 , 'rho':7.8, 'E1': 210, 'E2':220,'XT':600, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Steel"), 'fracture_thoughness':55, 'yield_strength':300} )
-materials.append( {'name':'Aluminium'             , 'rho':2.7, 'E1':  70, 'E2': 70,'XT':310, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Aluminium"), 'fracture_thoughness':33, 'yield_strength':270} )
-materials.append( {'name':'Copper alloy'          , 'rho':8.3, 'E1':  120, 'E2': 120,'XT':345, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Copper"), 'fracture_thoughness':1, 'yield_strength':310} )
-materials.append( {'name':'Titan '                , 'rho':4.5, 'E1':  110, 'E2': 110,'XT':1000, 'priceUSD_per_kg':115, 'fracture_thoughness':71, 'yield_strength':760} )
-materials.append( {'name':'Bamboo '               , 'rho':1.16, 'E1':  25, 'E2': 110,'XT':150, 'priceUSD_per_kg':0.94,} ) # https://dir.indiamart.com/impcat/bamboo-scaffolding.html?biz=10, https://www.bambooimport.com/en/what-are-the-mechanical-properties-of-bamboo
 
 
 
@@ -33,7 +25,7 @@ def find_stiffest_and_lightest(mat1,mat2):
         return mat1, mat2
 
 
-print(find_stiffest_and_lightest(materials[0],materials[1]))
+
 
 
 def find_strongest_and_lightest(mat1,mat2):
@@ -56,7 +48,7 @@ def find_strongest_and_lightest(mat1,mat2):
         print("The materials have identical properties")
         return mat1, mat2
 
-print(find_strongest_and_lightest(materials[0],materials[1]))
+#print(find_strongest_and_lightest(materials[0],materials[1]))
 
 def find_stiff_and_cheap(mat1,mat2):
     """
@@ -80,14 +72,13 @@ def find_stiff_and_cheap(mat1,mat2):
         return mat1, mat2
 
 
-print(find_stiff_and_cheap(materials[0],materials[1]))
+#print(find_stiff_and_cheap(materials[0],materials[1]))
 
 def compare_materials(names,xdata,ydata,xlabel,ylabel):
     """
            Makes a scatter plot of the different material values inputted.
            
            Inspired by https://folk.ntnu.no/nilspv/TMM4175/plot-gallery.html
-
            args: 
                 names: name of material
                 xdata: data along x-axis
@@ -105,4 +96,3 @@ def compare_materials(names,xdata,ydata,xlabel,ylabel):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     plt.show()
-
