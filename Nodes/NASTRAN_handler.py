aa59c5ab24640f293d2e59401ccc9dbda117415b
lerausenco <usencolera@gmail.com>
2021-04-15 16:41:18 +0200

    deals with empty list

--- Nodes/NASTRAN_handler.py
+++ Nodes/NASTRAN_handler.py
@@ -11,24 +11,31 @@ data = web_table_to_dictionary()
 materials=[]
 
 materials.append( {'name':'AISI_Steel_4340'       , 'rho':7.8, 'E1': 210, 'E2':220,'XT':600, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Steel"), 'fracture_thoughness':55, 'yield_strength':300} )
-materials.append( {'name':'Aluminium'             , 'rho':2.7, 'E1':  70, 'E2': 70,'XT':310, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Aluminium"), 'fracture_thoughness':33, 'yield_strength':270} )
-materials.append( {'name':'Copper alloy'          , 'rho':8.3, 'E1':  120, 'E2': 120,'XT':345, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Copper"), 'fracture_thoughness':1, 'yield_strength':310} )
-materials.append( {'name':'Titan '                , 'rho':4.5, 'E1':  110, 'E2': 110,'XT':1000, 'priceUSD_per_kg':115, 'fracture_thoughness':71, 'yield_strength':760} )
+materials.append( {'name':'Aluminum_2014'             , 'rho':2.7, 'E1':  70, 'E2': 70,'XT':310, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Aluminium"), 'fracture_thoughness':33, 'yield_strength':270} )
+materials.append( {'name':'Copper_C10100'          , 'rho':8.3, 'E1':  120, 'E2': 120,'XT':345, 'priceUSD_per_kg':find_dollar_cost_per_kg(data,"Copper"), 'fracture_thoughness':1, 'yield_strength':310} )
+materials.append( {'name':'Titanium_Alloy'                , 'rho':4.5, 'E1':  110, 'E2': 110,'XT':1000, 'priceUSD_per_kg':115, 'fracture_thoughness':71, 'yield_strength':760} )
 materials.append( {'name':'Bamboo '               , 'rho':1.16, 'E1':  25, 'E2': 110,'XT':150, 'priceUSD_per_kg':0.94,} ) # https://dir.indiamart.com/impcat/bamboo-scaffolding.html?biz=10, https://www.bambooimport.com/en/what-are-the-mechanical-properties-of-bamboo
 
 
 class NASTRAN_handler(myHandler):
     def do_GET(self):
         """
-            Respond to a GET request.
-            Images require their own file Content-type to be sent,
-            so the send_image function is used to be able to display them.
+            Respond to a GET request. Retrieves node list from fuseki and sorts
+            by "name" - which is date and time node was created. Takes the latest
+            node form the list and writes it to params.txt.
         """
         #query fuseki and get params
         json_data = query_fuseki()
         node_list = parse_json(json_data)
+        print(node_list)
+        node_list = sorted(node_list, key = lambda i: i['name'])
 
         # find desired material
+        if len(node_list)==0:
+            print("node list empty")
+            material = "Results pending"
+            return
+
         if node_list[-1]["material_pref"] == "stiff_light":
             material = find_stiffest_and_lightest(materials[0],materials[1])['name']
         elif node_list[-1]["material_pref"] == "strong_light":
